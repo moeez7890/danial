26f83fb1569571f3216e5b5c156128e6ba2deb84
moeez7890 <am9876456@gmail.com>
2024-12-24 23:32:39 +0500

    Delete sa.js

--- sa.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', () => {
-    document.querySelectorAll('section').forEach((section) => {
-        if (section.getBoundingClientRect().top < window.innerHeight) {
-            section.classList.add('visible');
-        }
-    });
-});
